c39775fb4d1e0677a734e2f44e896843e66ba81f
christianfdev <chriis_farias@hotmail.com>
2024-11-28 21:18:55 -0300

    primeira versão da atv-1

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,37 +1,58 @@
-import { randomUUID } from "crypto";
-import { sql } from '../db.js';
-import 'dotenv/config';
-
+import { Account } from "../models/Account.js";
 
 export class AuthController {
 
-    async list(){
 
-           return sql`select * from account where deleted_at is null`;
+
+    async list() {
+        return await Account.list();
+    }
+
+
+    async create(account) {
+        return await Account.create(account);
+    }
+
+
+    async update(accountId, accountUpdated) {
+        return await Account.update(accountId, accountUpdated);
+    }
+
+
+    async delete(accountId) {
+        return await Account.delete(accountId);
+    }
+
+
+
+
+    // async list(){
+
+    //        return sql`select * from account where deleted_at is null`;
        
-    }
+    // }
 
-    async create (account){
+    // async create (account){
 
-        const accountId = randomUUID();
+    //     const accountId = randomUUID();
 
-        const { nome, email, senha } = account;
+    //     const { nome, email, senha } = account;
 
-        await sql`insert into account (id, nome, email, senha) VALUES (${accountId}, ${nome}, ${email}, ${senha})`;
+    //     await sql`insert into account (id, nome, email, senha) VALUES (${accountId}, ${nome}, ${email}, ${senha})`;
 
-    }
+    // }
 
-    async update (accountId, accountUpdated){
+    // async update (accountId, accountUpdated){
         
-        const { nome, email, senha } = accountUpdated;
+    //     const { nome, email, senha } = accountUpdated;
 
-        await sql`update account set nome = ${nome}, email = ${email}, senha = ${senha} where id = ${accountId}`
+    //     await sql`update account set nome = ${nome}, email = ${email}, senha = ${senha} where id = ${accountId}`
 
-    }
+    // }
 
-    async delete (accountId){
+    // async delete (accountId){
 
-        await sql`update account set deleted_at = now() where id = ${accountId}`;
+    //     await sql`update account set deleted_at = now() where id = ${accountId}`;
 
-    }
+    // }
 }
--- src/models/Account.js
+++ src/models/Account.js
@@ -1,3 +1,6 @@
+import { sql } from '../db.js';
+import { randomUUID } from "crypto";
+import 'dotenv/config';
 
 export class Account {
     id;
@@ -13,7 +16,35 @@ export class Account {
     }
 
 
-    
+    static async list(){
+
+        return sql`select * from account where deleted_at is null`;
+           
+    }
+
+    static async create (account){
+
+        const accountId = randomUUID();
+
+        const { nome, email, senha } = account;
+
+        await sql`insert into account (id, nome, email, senha) VALUES (${accountId}, ${nome}, ${email}, ${senha})`;
+
+    }
+
+    static async update (accountId, accountUpdated){
+        
+        const { nome, email, senha } = accountUpdated;
+
+        await sql`update account set nome = ${nome}, email = ${email}, senha = ${senha} where id = ${accountId}`
+
+    }
+
+    static async delete (accountId){
+
+        await sql`update account set deleted_at = now() where id = ${accountId}`;
+
+    }
 
 }
 
