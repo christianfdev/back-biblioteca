39108f987db56f1795f9ab4b385d59cd88af5e88
christianfdev <chriis_farias@hotmail.com>
2024-12-06 18:52:50 -0300

    Removal of comments

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -2,8 +2,6 @@ import { Account } from "../models/Account.js";
 
 export class AuthController {
 
-
-
     async list() {
         return await Account.list();
     }
@@ -23,36 +21,4 @@ export class AuthController {
         return await Account.delete(accountId);
     }
 
-
-
-
-    // async list(){
-
-    //        return sql`select * from account where deleted_at is null`;
-       
-    // }
-
-    // async create (account){
-
-    //     const accountId = randomUUID();
-
-    //     const { nome, email, senha } = account;
-
-    //     await sql`insert into account (id, nome, email, senha) VALUES (${accountId}, ${nome}, ${email}, ${senha})`;
-
-    // }
-
-    // async update (accountId, accountUpdated){
-        
-    //     const { nome, email, senha } = accountUpdated;
-
-    //     await sql`update account set nome = ${nome}, email = ${email}, senha = ${senha} where id = ${accountId}`
-
-    // }
-
-    // async delete (accountId){
-
-    //     await sql`update account set deleted_at = now() where id = ${accountId}`;
-
-    // }
 }
